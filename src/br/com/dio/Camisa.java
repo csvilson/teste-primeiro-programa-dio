45969b13397172e807d21394268bb0779402943c
csvilson <csvilson@gmail.com>
2023-03-02 12:10:07 -0300

    Primeiro programa teste do curso da DIO

---
+++ src/br/com/dio/Camisa.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+public class Camisa {
+	
+	public static void main(String[] args){
+		
+		String tamanho = "M";
+		String cor = "vermelha";
+		int preco = 80;
+		
+		System.out.println("Minha camisa tem o temanho " + tamanho + ", a cor " + cor + " e o valor de " + preco+ " reais");
+	}
+	
+	
+	
+}//Camisa
